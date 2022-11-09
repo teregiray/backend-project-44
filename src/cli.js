ff493b3778fe6d671446b7469133092183d62d31
Андрей Руденко <dantonengier@mail.ru>
2022-11-09 21:36:09 +0300

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
+var name = readlineSync.question('May I have your name? ');
 export function askName() {
   return (`Hello, ${name}!`);
 }
+
