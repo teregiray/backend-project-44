da4e19f5db2335dc3d1f48621e378e86cba361f7
Андрей Руденко <dantonengier@mail.ru>
2022-11-09 22:09:39 +0300

    fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,1 +1,11 @@
+const function gameEven = (something) => {
+let randomNumber = Math.floor(Math.random()*100)
+let evenBoolean = randomNumber % 2 === 0;
+let question = "Question: ";
+let result = false;
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+console.log(question + number)
+while (result !== true) {
 
+}
+}
