264dc3416c09475f3d8e146e985796566d5cd4e1
Андрей Руденко <dantonengier@mail.ru>
2022-11-12 14:13:43 +0300

    fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,14 +1,8 @@
 import greeting from '../src/cli.js' ;
 greetingFunction();
+const isEven = () => {
 const randomNumber = Math.floor(Math.random()*100)
-
-const function gameEven = (something) => {
-let evenBoolean = randomNumber % 2 === 0;
-let question = "Question: ";
-let result = false;
+const evenAnswer = randomNumber % 2 === 0
+console.log( "Question: " + randomNumber;
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
-console.log(question + number)
-while (result !== true) {
-
-}
 }
