0d74fcc76613d3406c3881a7d68eb23bf3ce428f
Андрей Руденко <dantonengier@mail.ru>
2022-11-12 14:22:29 +0300

    add games/

---
+++ games/brain-even.js
@@ -0,0 +1,8 @@
+import greeting from '../src/cli.js' ;
+greetingFunction();
+const isEven = () => {
+const randomNumber = Math.floor(Math.random()*100)
+const evenAnswer = randomNumber % 2 === 0
+console.log( "Question: " + randomNumber;
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+}
