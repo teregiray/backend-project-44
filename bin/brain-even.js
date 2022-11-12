dddd3f91a3dd3632e6fb593a3aedd2fc2a14c6cb
Андрей Руденко <dantonengier@mail.ru>
2022-11-12 14:22:48 +0300

    fix

--- bin/brain-even.js
+++
@@ -1,8 +0,0 @@
-import greeting from '../src/cli.js' ;
-greetingFunction();
-const isEven = () => {
-const randomNumber = Math.floor(Math.random()*100)
-const evenAnswer = randomNumber % 2 === 0
-console.log( "Question: " + randomNumber;
-console.log('Answer "yes" if the number is even, otherwise answer "no".')
-}
