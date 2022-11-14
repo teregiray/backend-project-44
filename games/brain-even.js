5f095035131df4ef91c78dabfa044ea775b684eb
teregiray <113193752+teregiray@users.noreply.github.com>
2022-11-14 12:02:47 +0300

    Update brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,7 +1,6 @@
-import greeting from '../src/cli.js' ;
-greetingFunction();
+import {greeting} from '../src/cli.js' ;
+greeting();
 const isEven = () => {
-const randomNumber = Math.floor(Math.random()*100)
 const evenAnswer = randomNumber % 2 === 0
 console.log( "Question: " + randomNumber;
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
