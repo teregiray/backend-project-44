bf5d1b0b950d7a29fed4050c713e6eec5a0a81e5
teregiray <dantonengier@mail.ru>
2022-11-15 10:14:54 +0300

    fix

--- src/games/isEven.js
+++ src/games/isEven.js
@@ -1,8 +1,8 @@
-import {greeting} from '../../cli.js' ;
-import {randomNumber} from '../../index.js' ; 
+import {greeting} from '../cli.js' ;
+import {randomNumber} from '../index.js' ; 
 greeting();
 const isEven = () => {
 const evenAnswer = randomNumber % 2 === 0
-console.log( "Question: " + randomNumber;
+console.log( "Question: " + randomNumber);
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
 }
