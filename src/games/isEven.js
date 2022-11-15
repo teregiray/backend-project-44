9de429249e58033a2bc03fbf89c9bf49d96cf5cd
teregiray <dantonengier@mail.ru>
2022-11-15 10:33:40 +0300

    fix

--- src/games/isEven.js
+++ src/games/isEven.js
@@ -9,7 +9,7 @@ const evenAnswer = randomNumber % 2 === 0
 let answersCounter = 0;
 while(answersCounter < 3) {
 console.log( "Question: " + randomNumber);
-const answerUser = readline.question("Question: " + randomNumber);
+const answerUser = readlineSync.question("Your answer: ");
 if (answerUser === "yes" && evenAnswer === true) {
 	answersCounter =+1;
 	console.log("Correct!");
