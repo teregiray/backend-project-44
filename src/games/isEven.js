37348bf022f8d15740c0be181659a281f16190fd
teregiray <dantonengier@mail.ru>
2022-11-16 11:03:01 +0300

    fix

--- src/games/isEven.js
+++ src/games/isEven.js
@@ -2,32 +2,36 @@ import readlineSync from 'readline-sync';
 import {greeting} from '../cli.js' ;
 greeting();
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
-let answersCounter = 0;
+export function isEven()  {
+let answersCounter = 0
 while(answersCounter < 3) {
 	const randomNumber = Math.floor(Math.random() * 100);
 	const evenAnswer = randomNumber % 2 === 0
-console.log("Question: " + randomNumber);
-const answerUser = readlineSync.question("Your answer: ");
-
+	console.log("Question: " + randomNumber);
+	const answerUser = readlineSync.question("Your answer: ");
 if (answerUser === "yes" && evenAnswer === true) {
 	console.log("Correct!");
 	answersCounter =+1;
 }
+
 else if (answerUser === "yes" && evenAnswer === false) {
-console.log(answerUser + " is wrong answer  ;(. Correct answer was 'no'. \nLet's try again, Bill!")
-break;
-}
-else if (answerUser === "no" && evenAnswer  === false ) {
+	console.log(answerUser + " is wrong answer  ;(. Correct answer was 'no'. \nLet's try again, Bill!")
+	return
+	}
+
+	if (answerUser === "no" && evenAnswer  === false ) {
 	console.log("Correct!");
 	answersCounter +=1;
 }
-else if (answerUser === "no" && evenAnswer === true){
+
+else if (answerUser === "no" && evenAnswer === true) {
 console.log(answerUser + " is wrong answer  ;(. Correct answer was 'yes'. \nLet's try again, Bill!")
-break;
+return
 }
 
 }
 if (answersCounter === 3) {
 console.log("Congratulations, Bill"  ) // придумать как эспортировать переменную из cli.js и заменить на "Bill"
 }
-
+}
+isEven()
