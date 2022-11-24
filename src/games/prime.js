2fb9cdf3f8f24b6a07a58964e84b75bbc4acf4cd
Андрей Руденко <dantonengier@mail.ru>
2022-11-24 13:53:55 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,6 +6,7 @@ console.log(`Hello, ${userName}!`);
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
 export function prime()  {
     let result = 0;
+    let resultLength = 0;
     let answersCounter = 0;
   while(answersCounter < 3) {
     let randomNumber1 = Math.floor(Math.random() *10 +1)
@@ -17,19 +18,19 @@ export function prime()  {
         }
     
     }
-    let resultLength = result.length === 2
-    if(resultLength === true && answerUser === "yes") {
-        console.log("Correct!")
+    if (result.length == 2 ) {
         resultLength = "yes"
-        answersCounter +=1;
     }
-    if(resultLength === false && answerUser === 'no') {
-        console.log("Correct!")
+    else {
         resultLength = "no"
+    }
+
+    if (resultLength === answerUser) {
+        console.log("Correct!");
         answersCounter +=1;
     }
     else {
-        console.log(answerUser + ` is wrong answer  ;(. Correct answer was ${resultLength}. \nLet's try again, ${userName}!`);
+        console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${resultLength}'.\nLet's try again, ${userName}!`);
         break;
     }
 
