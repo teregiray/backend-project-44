d4d9700fa5ae3a3854118b671f51a9374e0c4c53
Андрей Руденко <dantonengier@mail.ru>
2022-11-24 14:17:55 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,39 +3,44 @@ import name from '../cli.js' ;
 console.log('Welcome to the Brain Games!');
 const userName = name()
 console.log(`Hello, ${userName}!`);
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
-export function prime()  {
-    let result = 0;
-    let resultLength = 0;
-    let answersCounter = 0;
-  while(answersCounter < 3) {
-    let randomNumber1 = Math.floor(Math.random() *10 +1)
-    console.log("Question: " + randomNumber1);
-    let answerUser = readlineSync.question("Your answer: ");
-    for(let i = 0 ; i < randomNumber1 ; i +=1) {
-        if(randomNumber1 % i === 0) {
-            result += String(i)
-        }
+console.log('What number is missing in the progression?')
+export function prime() {
+
+
+  
+  let answersCounter = 0;
+    while(answersCounter < 3) {
+    let rightAnswer;
+    const result = [];
+    const randomNumber1 = Math.floor(Math.random() * 10 );
+    for (let z = 0; z <= randomNumber1; z += 1) {
+      if (randomNumber1 % z === 0) {
+        result.push(z);
+      }
+    }
     
+   
+    if (result.length === 2) {
+      rightAnswer = 'yes';
+    } else {
+      rightAnswer = 'no';
     }
-    if (result.length == 2 ) {
-        resultLength = "yes"
-    }
-    else {
-        resultLength = "no"
-    }
+    
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+    console.log(`Question: ${randomNumber1}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    
 
-    if (resultLength === answerUser) {
-        console.log("Correct!");
-        answersCounter +=1;
+    if (rightAnswer === userAnswer) {
+      console.log('Correct!');
+      answersCounter +=1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. \nLet's try again, ${userName}!`);
+      return;
     }
-    else {
-        console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${resultLength}'.\nLet's try again, ${userName}!`);
-        break;
-    }
+  }
+  if(answersCounter === 3) {
+  console.log(`Congratulations, ${userName}!`);
+}
+}
 
-}
-    if(answersCounter == 3) {
-console.log(`Congratulations, ${userName}! `) 
-    }
-}
