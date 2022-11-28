50aa96c95e59c00c516d45678e8a8c7338988037
Андрей Руденко <dantonengier@mail.ru>
2022-11-28 13:24:17 +0300

    fix

---
+++ src/engine.js
@@ -0,0 +1,26 @@
+
+
+import readlineSync from 'readline-sync';
+
+const roundCount = 3;
+
+export const engine = (gameFunction, description) => () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+  console.log(description);
+  for (let i = 0; i < roundCount; i += 1) {
+    const [question, answer] = gameFunction();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== answer) {
+      console.log(`'${userAnswer}'is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default engine;
