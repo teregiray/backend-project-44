995d28be2c960ba0d2e8ed493df822871eace244
teregiray <@dantonengier@mail.ru>
2023-01-20 15:29:57 +0300

    isEven

--- src/games/isEven.js
+++ src/games/isEven.js
@@ -4,3 +4,14 @@ import  generateRandomNumber  from '../generateRandomNumber.js';
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => number % 2 === 0;
+
+const gameFunction = () => {
+    const number = generateRandomNumber(1,99);
+    const question = `${number}`;
+    const answer = isEven(number) ? "yes" : "no";
+    return question, answer
+}
+
+export const playGame = engine(gameFunction, gameDescription);
+export default playGame
