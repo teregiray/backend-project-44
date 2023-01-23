182117e47e55b3def51c73e7268d6e2f0c98253d
teregiray <@dantonengier@mail.ru>
2023-01-23 13:15:18 +0300

    calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,32 +4,26 @@ import  generateRandomNumber  from '../generateRandomNumber.js';
 
 const gameDescription = 'What is the result of the expression?';
 
-const signs = ["*","+","-"]
-const randomSign = signs[Math.floor(Math.random() * signs.length)];
-
 const theCalc = (number1, number2, randomSign) => {
     switch (randomSign) {
         case '*':
-          answer = number1 * number2;
-          break;
+          return number1 * number2;
         case '-':
-          answer = number1 - number2;
-          break;
-        default:
-          answer = number1 + number2;
-          break;
+          return number1 - number2;
+        case "+":
+          return number1 + number2;
       }
-      return answer
 }
 
 
 const gameFunction = () => {
-    const number1 = generateRandomNumber(1,99);
-    const number2 = generateRandomNumber(1,99);
+    const signs = ["*","+","-"]
+    const randomSign = signs[Math.floor(Math.random() * signs.length)];
+    const number1 = generateRandomNumber(1,10);
+    const number2 = generateRandomNumber(1,10);
     const question = `${number1} ${randomSign} ${number2}`;
-    const answer = theCalc(number1, number2, randomSign);
+    const answer = String(theCalc(number1, number2, randomSign));
     return [question, answer]
-    console.log(question)
 }
 
 export const playGame = engine(gameFunction, gameDescription);
