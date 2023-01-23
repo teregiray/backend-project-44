aacb4da877b7c74cdb5eafdc27fc35395d0ece15
teregiray <@dantonengier@mail.ru>
2023-01-23 13:25:40 +0300

    fix

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,3 +4,20 @@ import  generateRandomNumber  from '../generateRandomNumber.js';
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
+const isGcd = (number1, number2) => {
+    if (number2) {
+        return isGcd(number2, number1 % number2);
+    } else {
+        return number1;
+    }
+}
+const gameFunction = () => {
+const number1 = generateRandomNumber(1,49);
+const number2 = generateRandomNumber(1,49);
+const question = `${number1} ${number2}`;
+const answer = String(isGcd(number1, number2));
+return [question, answer]
+};
+
+export const playGame = engine(gameFunction, gameDescription);
+export default playGame
